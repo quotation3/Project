0f68af7431241512097d941fcbfc9ab30918b103
quotation <mancity0912@gmail.com>
2020-08-28 11:01:14 +0900

    스크래피 미완성

---
+++ Instagram/InstaCrawl/InstaCrawl/spiders/InstaProfile.py
@@ -0,0 +1,31 @@
+import re, requests, json, urllib, datetime, scrapy
+import pandas as pd
+from Crawling.items import InstacrawlItem
+
+class InstaProfileSpider(scray, Spider):
+    name = 'insta_profile'
+    # allowed_domains = ['instagram.com']
+    # start_urls = ['http://instagram.com/']
+    url_format = 'https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables=%7B%22user_id%22%3A%22{}%22%2C%22include_chaining%22%3Afalse%2C%22include_reel%22%3Atrue%2C%22include_suggested_users%22%3Atrue%2C%22include_logged_out_extras%22%3Afalse%2C%22include_highlight_reels%22%3Afalse%2C%22include_live_status%22%3Atrue%7D'
+    crawling_count = 0
+    def __init__(self, file):
+        user_id = list(pd.read_csv(file, header=None)[0])
+        self.start_urls = []
+        for user in user_id:
+            self.start_urls.append(InstaProfileSpider.url_format.format(user))
+
+    def parse(self, response):
+        result_dic = response.json()
+        item = InstacrawlItem
+        for i in range(len(result_dic['data']['user']['reel']['user']['username'])):
+            # 크롤링 횟수 카운트
+            self.crawling_count  += 1
+            if self.crawling_count > 200:
+                time.sleep(300)
+            item['login_id'] = result_dic['data']['user']['reel']['user']['username']
+
+            response_profile = requests.get('https://www.instagram.com/{}/?__a=1'.format(login_id))
+            json = response_profile.json()
+            item['profile'] = json['graphql']['user']['biography']
+            item['follower'] = json['graphql']['user']['edge_followed_by']['count']
+            item['following'] = json['graphql']['user']['edge_follow']['count']
